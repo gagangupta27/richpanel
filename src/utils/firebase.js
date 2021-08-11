1df764d437ca0afb62cdc6b72b70d508a9f5c2d3
gagan gupta <gupta.gag27@gmail.com>
2021-08-11 15:42:41 +0530

    readme

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,7 +2,13 @@ import firebase from "firebase";
 
 if (!firebase.apps.length) {
   const firebaseApp = firebase.initializeApp({
-
+    apiKey: "AIzaSyDc5n0FDNOPbugNMEguxVmlAwzndOMg_Bs",
+    authDomain: "richpanel-ae850.firebaseapp.com",
+    databaseURL: "https://richpanel-ae850-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "richpanel-ae850",
+    storageBucket: "richpanel-ae850.appspot.com",
+    messagingSenderId: "476312510637",
+    appId: "1:476312510637:web:4d29547d913f3c2bf3a624"
   });
 } else {
   firebase.app();
